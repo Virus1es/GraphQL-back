20351e209eabd675e661e5a8c5bb026cbc41ae6a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-26 00:09:16 +0300

    feat: create func to create user by graphql

--- index.js
+++ index.js
@@ -9,12 +9,24 @@ const users = [
 const app = express();
 app.use(cors());
 
+const createUser = (input) => {
+    const id = Date.now();
+    return {
+        id, ...input
+    }
+};
+
 const root = {
     getAllUsers: () => {
         return users;
     },
     getUsers: ({id}) => {
         return users.find(user => user.id == id)
+    },
+    createUser: ({input}) => {
+        const user = createUser(input);
+        users.push(user);
+        return user;
     }
 }
 
